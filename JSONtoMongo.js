6a135a030e253132d61584389347d3faf484aa07
Reid Mollway <reidmollway@gmail.com>
2017-08-29 15:40:32 -0400

    Assignment3 Functional

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,40 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
 /* Connect to your database */
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+mongoose.connect(config.db.uri);
+
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
+fs.readFile('listings.json', 'utf-8', function(err, data) {
 
+    var currentListing;
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+    if(err) throw err;
+
+    var listingData = JSON.parse(data);
+    var numListings = Object.keys(listingData.entries).length;
+
+    for(var i=0; i<numListings; i++) {
+      currentListing = new Listing(listingData.entries[i]);
+      currentListing.save(function(err, listing) {
+        
+      });
+    }
+});
+
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- queries.js
+++ queries.js
@@ -1,31 +1,88 @@
 /* Fill out these functions using Mongoose queries*/
 
+var Listing = require('./ListingSchema.js'),
+    mongoose = require('mongoose'),
+    config = require('./config');
+
+mongoose.connect(config.db.uri);
+
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.findOne({name: 'Library West'}, function(err, listing){
+     if(err) {
+       console.log('Error on Lib West');
+       throw err;
+     }
+     console.log('\n\nLibrary West Data\n');
+     console.log(listing);
+     console.log('\n\n');
+   });
+
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.findOneAndRemove({code: 'CABL'}, function(err){
+     if(err) throw err;
+
+     console.log('\n\nCable Document Deleted \n\n');
+
+     /*
+        Double-checking it's been removed by finding and expecting a null result
+     */
+     Listing.findOne({code: 'CABL'}, function(err, result){
+        if(result != null) console.log('\nFound CABL after deletion!\n');
+        else console.log('\nCABL not found after deletion\n');
+     });
+
+   });
+
 };
-var updatePhelpsMemorial = function() {
+var updatePhelpsLab = function() {
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+
+   /*
+    After some research, it appears the proper address is as follows:
+      1275 Center Dr, Gainesville, FL 32610
+
+    Which corresponds to the following coordinates:
+      [29.6406417,-82.34565069999996]
+   */
+
+   Listing.findOneAndUpdate({ code : 'PHL'}, {"$set": { address: '1275 Center Dr, Gainesville, FL 32610', updated_at: new Date(),  coordinates: [{latitude: 29.6406417, longitude: -82.34565069999996}]}}, function(err, listing){
+     if(err) throw err;
+
+     console.log('\n\nUpdated Phelps Lab: \n');
+     console.log(listing);
+     console.log('\n\n');
+   });
+
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err, allListings){
+     if(err){
+       console.log('Error in RetrieveAll: '+ err);
+       throw err;
+     }
+     console.log('\n\nAll listings\n');
+     console.log(JSON.stringify(allListings));
+     console.log('\n\n');
+   });
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
