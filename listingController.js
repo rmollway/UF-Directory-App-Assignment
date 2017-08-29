454f3e6f346a8332986b8ab35047db6d0948fbd0
Reid Mollway <reidmollway@gmail.com>
2017-08-29 10:42:27 -0400

    Using more icons, added auto-hide feature for detailed info

--- listingController.js
+++ listingController.js
@@ -4,11 +4,16 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
     $scope.newListing = undefined;
 
+    angular.forEach(Listings, function(listing){
+      listing.isClicked = false;
+    });
+
     /*
       Implement these functions in the controller to make your application function
       as described in the assignment spec.
      */
     $scope.addListing = function() {
+      this.newListing.isClicked = false;
       $scope.listings.push(this.newListing);
       $scope.listings.sort(function(a,b){ return (a.code > b.code) ? 1 : (a.code < b.code) ? -1 : 0;});
       $scope.newListing = undefined;
@@ -21,7 +26,11 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
     $scope.showDetails = function(listingEntry) {
       $scope.detailedInfo = listingEntry;
+      listingEntry.isClicked = true;
+      angular.forEach($scope.listings, function(listing) {
+        if(listing.code != listingEntry.code) listing.isClicked = false;
+      });
     };
-    
+
   }
 ]);
